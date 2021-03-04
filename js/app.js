137152abfc0fff4a38d4199aa82afe83f58bf843
unknown <sushmitha.svk@gmail.com>
2021-03-04 12:46:51 +0530

    Finally done along with pwa

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+if ('serviceWorker' in navigator) {
+  // register service worker
+  navigator.serviceWorker.register('service-worker.js');
+}
 var itemList = document.getElementById("notes");
 
 itemList.addEventListener("click", removeItem);
